5ff3e114bf0241ae4f5f704448f5c6b496e455c1
Bayram Salman <s.bayram.1037@gmail.com>
2025-01-03 10:33:09 +0300

    Initial commit

---
+++ Entity/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace efBlogApp.Entity
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
